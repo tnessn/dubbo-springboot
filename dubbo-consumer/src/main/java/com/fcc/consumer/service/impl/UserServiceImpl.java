7f3babed0e9c61ba2e76d85aef98cfef29eddb8b
huangjinfeng <1005886607@qq.com>
2019-04-01 14:34:43 +0800

    update

--- dubbo-consumer/src/main/java/com/fcc/consumer/service/impl/UserServiceImpl.java
+++ dubbo-consumer/src/main/java/com/fcc/consumer/service/impl/UserServiceImpl.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Component;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.alibaba.dubbo.config.annotation.Service;
-import com.alibaba.fescar.spring.annotation.GlobalTransactional;
 import com.fcc.api.service.DemoService;
 import com.fcc.consumer.service.UserService;
 
@@ -20,7 +19,6 @@ public class UserServiceImpl implements UserService{
 	
 
 	@Override
-	 @GlobalTransactional(timeoutMills = 300000, name = "dubbo-gts-fescar-example")
 	public void updateUser(String age) {
 		demoService.updateUser(age);		
 	}
